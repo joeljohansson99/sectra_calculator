9492ef7e752bd12ae9372977394ce3e27140bb34
Joel Johansson <joel.k.johansson@gmail.com>
2023-02-18 11:50:18 -0500

    added file as input support

--- calculator.py
+++ calculator.py
@@ -24,13 +24,43 @@ def eval(reg, regs, ops):
 
     regs[reg] = (sum, True)
 
+def parse_line(input, regs, ops):
+    if input[0] == "print":
+        reg = input[1]
+        (val, updated) = regs[reg]
+
+        if not updated:
+            eval(reg, regs, ops)
+            (val, _) = regs[reg]
+
+        print(val)
+    else:
+        [reg, op, param] = input
+
+        if reg in ops:
+            ops[reg].append((op, param))
+
+            (val, updated) = regs[reg]
+
+            if updated:
+                regs[reg] = (val, False)
+        else:
+            regs[reg] = (0, False)
+            ops[reg] = [(op, param)]
+
 def main():
 
     regs = dict()
     ops = dict()
 
     if len(sys.argv) > 1:
-        file = sys.argv[1]
+        filename = sys.argv[1]
+        with open(filename) as file:
+            lines = [line.rstrip().lower() for line in file]
+
+            for line in lines:
+                input = line.split(" ")
+                parse_line(input, regs, ops)
 
     else:
         for line in sys.stdin:
@@ -39,29 +69,7 @@ def main():
             if input[0] == "quit":
                 break
 
-            if input[0] == "print":
-                reg = input[1]
-                (val, updated) = regs[reg]
-
-                if not updated:
-                    eval(reg, regs, ops)
-                    (val, _) = regs[reg]
-
-                print(val)
-
-            else:
-                [reg, op, param] = input
-
-                if reg in ops:
-                    ops[reg].append((op, param))
-
-                    (val, updated) = regs[reg]
-
-                    if updated:
-                        regs[reg] = (val, False)
-                else:
-                    regs[reg] = (0, False)
-                    ops[reg] = [(op, param)]
+            parse_line(input, regs, ops)
     
     return 0
 
