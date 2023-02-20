1b6e89b5f252e2ffad88c50baa97464b2e28236b
Joel Johansson <joel.k.johansson@gmail.com>
2023-02-19 20:34:48 -0500

    Added comments and updated README

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# sectra_calculator
+# Sectra Calculator Assignment
+
+Simple calculator that can add, subtract and multiply values in a set of registers.
+
+## Prerequisites
+
+* Python 3 - https://www.python.org/downloads/
+
+## Installation
+
+```
+git clone https://github.com/joeljohansson99/sectra_calculator.git
+```
+
+## Run
+
+The calculator can either take console input, or a file as input.
+
+To run with console input:
+```
+python3 calculator.py
+```
+
+To run with file input:
+```
+python3 calculator.py <file>
+```
+
+## Usage 
+
+Syntax is as following:
+```
+<register> <operation> <value>
+print <register>
+quit
+```
+
+Registers is defined as any combination of alphanumeric characters.
+
+All registers contains the value zero from start.
+
+Allowed operations are `add`, `subtract` and `multiply`.
--- calculator.py
+++ calculator.py
@@ -3,18 +3,23 @@ import sys
 # This function evaluates a register value
 def eval(reg, regs, ops):
     (sum, _) = regs[reg]
-
+    
+    # Loop through the pending operations
     while len(ops[reg]) > 0:
         (op, param) = ops[reg].pop(0)
 
+        # Check whether the parameter is a register or value
         if param.isnumeric():
             val = int(param)
         else: 
+            # If no operations have been made in the specified register,
+            # it is added to regs dictionary with value zero
             if param not in regs:
                 regs[param] = (0, True)
 
             (val, updated) = regs[param]
 
+            # If the value is not updated then it is evaluated first
             if not updated:
                 eval(param, regs, ops)
                 (val, _) = regs[param]
@@ -27,8 +32,9 @@ def eval(reg, regs, ops):
         elif op == "multiply":
             sum *= val
         else:
-            print("Invalid operand: " + op)
+            print("Invalid operation: " + op)
 
+    # Lastly write the new value to the register and flag it as up-to-date
     regs[reg] = (sum, True)
 
 # This function parses a command
@@ -36,28 +42,34 @@ def parse_command(cmd, regs, ops):
     if cmd[0] == "print":
         reg = cmd[1]
 
+        # If no operations have been made in the specified register,
+        # it is added to regs dictionary with value zero
         if reg not in regs:
             regs[reg] = (0,True)
 
         (val, updated) = regs[reg]
 
+        # If the the registers value is not up-to-date then evaluate it first
         if not updated:
             eval(reg, regs, ops)
             (val, _) = regs[reg]
 
         print(val)
-
+    
     else:
         [reg, op, param] = cmd
 
+        # Add pending operation to register, and set the updated field to false
         if reg in ops:
             ops[reg].append((op, param))
 
             (val, updated) = regs[reg]
 
+            # If the register had already been flagged as not up-to-date, then no need to write to it again
             if updated:
                 regs[reg] = (val, False)
         else:
+            # If the register is not in regs dictionary, add it with value zero and flagged as not up-to-date
             regs[reg] = (0, False)
             ops[reg] = [(op, param)]
 
@@ -65,6 +77,7 @@ def main():
     regs = dict() # stores value and updated field for each register
     ops = dict() # stores (pending) operations for each register
 
+    # Reads input from either file or console
     if len(sys.argv) > 1:
         filename = sys.argv[1]
         with open(filename) as file:
@@ -74,7 +87,9 @@ def main():
                 try:
                     parse_command(cmd, regs, ops)
                 except:
+                    # Will catch any input that does not match syntax
                     print("Invalid command: " + line.rstrip())
+
     else:
         for line in sys.stdin:
             cmd = line.rstrip().lower().split(" ")
@@ -84,6 +99,7 @@ def main():
             try:
                 parse_command(cmd, regs, ops)
             except:
+                # Will catch any input that does not match syntax
                 print("Invalid command: " + line.rstrip())
     
     return 0
